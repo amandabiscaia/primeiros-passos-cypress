0025597c22d08e12f286cae55ee7141bbd331b97
User <user@Users-MacBook-Pro.local>
2024-11-07 09:59:14 -0300

    add ramdon values on user spec chancejs

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,33 +1,48 @@
 import userData from '../fixtures/user-data.json'
-import LoginPage from '../pages/loginPage'
+import LoginPage from '../pages/loginPage' 
 import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 
-const Chance = require('chance')
 
-const chance = new Chance();
-const loginPage = new LoginPage
+const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
 const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
 
-  it('User Info Update - Success', () => {
+  const selectorsList = {
+    
+    
+  }
+  
+ it('User Info Update - Success', () => {
     loginPage.accesLoginPage()
     loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
 
     dashboardPage.checkDashboardPage()
-
+  
     menuPage.accesMyInfo()
 
-    myInfoPage.fillPersonalDetails(chance.first(), chance.last())
-    myInfoPage.fillEmployeeDetails(chance.string({ length: 5 }), chance.string(), chance.cpf(), '2025-07-29')
+    myInfoPage.fillPersonalDetails('First Name','Last Name')
+    myInfoPage.fillEmployeeDetails('EmployId', 'otherId', 'Drivers Number', '2025-07-29')
     myInfoPage.fillStatus()
     myInfoPage.saveForm()
     
-  })
- 
-})
+
+
   
+  
+  })
+  it('Login - Fail', () => {
+    loginPage.accesLoginPage()
+    loginPage.loginWithUser(userData.userFail.username, userData.userFail.password)
+    loginPage.checkAccessInvalid()
+
+    
+    
+    
+  })
+  
+})
